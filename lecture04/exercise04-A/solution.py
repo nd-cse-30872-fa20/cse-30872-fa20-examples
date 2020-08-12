2e8967df8b9af33cb6ed96d02eadf20b825f1ee3
Peter Bui <pbui@nd.edu>
2020-08-12 17:06:06 -0400

    Add exercise04-A

---
+++ lecture04/exercise04-A/solution.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import collections
+import sys
+
+# Functions
+
+def is_anagram_sorted(s, t):
+    return sorted(s.lower()) == sorted(t.lower())
+
+def is_anagram_histogram(s, t):
+    sc = collections.Counter(s.lower())
+    tc = collections.Counter(t.lower())
+    return sc == tc
+
+is_anagram = is_anagram_histogram   # Select on of the solutions
+
+# Main Execution
+
+def main():
+    for line in sys.stdin:
+        first, second = line.strip().split()
+        print('Anagram' if is_anagram(first, second) else 'Not Anagram')
+
+if __name__ == '__main__':
+    main()
